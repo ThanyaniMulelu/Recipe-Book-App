eae4b2f3c9005a7527fc131655a029a4cb7959ec
ThanyaniMulelu <141078852+ThanyaniMulelu@users.noreply.github.com>
2024-03-01 14:55:54 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,57 @@
+
+const API_KEY = "f5414560ee9d4395b3487ce2ee900ce4";
+const recipeListEL = document.getElementById("recipe-list");
+
+function displayRecipes(recipes) {
+recipeListEL.innerHTML = ""
+recipes.forEach((recipe) => {
+
+    const recipeItemEL = document.createElement("li");
+    recipeItemEL.classList.add("recipe-item");
+    recipeImageEL = document.createElement("img");
+    recipeImageEL.src = recipe.image;
+    recipeImageEL.alt = "recipe image";
+
+    recipeTitleEL = document.createElement("h2");
+    recipeTitleEL.innerText = recipe.title;
+
+    recipeIngredientsEL = document.createElement("p");
+    recipeIngredientsEL.innerHTML = `
+    
+    <strong>Inggredients:</strong> ${recipe.extendedIngredients.map((ingredient)=>ingredient.original).join(", ")}
+    
+    `;
+
+    recipeLinkEL = document.createElement("a");
+    recipeLinkEL.href = recipe.sourceUrl;
+    recipeLinkEL.innerText = "View Recipe";
+
+    recipeItemEL.appendChild(recipeImageEL)
+    recipeItemEL.appendChild(recipeTitleEL)
+    recipeItemEL.appendChild(recipeIngredientsEL)
+    recipeItemEL.appendChild(recipeLinkEL)
+
+    recipeListEL.appendChild(recipeItemEL);
+
+});
+
+}
+async function getRecipes() {
+    const response = await fetch(`https://api.spoonacular.com/recipes/random?number=10&apiKey=${API_KEY}`)
+
+    const data = await response.json()
+
+    return data.recipes;
+
+}
+
+
+async function init(){
+
+    const recipes = await getRecipes();
+    displayRecipes(recipes)
+   
+
+}
+
+init() ;
